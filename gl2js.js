1da584d802e5fa1225b30f35170fb9b985b51c21
Iván Sánchez Ortega <ivan@mazemap.no>
2015-06-12 15:13:52 +0200

    Hack glsl-unit internals to keep map of global variable names between files

--- gl2js.js
+++ gl2js.js
@@ -1,6 +1,57 @@
 
 var glslunit = require('./lib/glsl-compiler');
 
+
+
+// Override glslunit.compiler.VariableMinifier.prototype.performStep so it reuses
+//   the variable map for the top level of all of our files.
+var globalNameGenerator = new glslunit.compiler.NameGenerator();
+
+glslunit.compiler.VariableMinifier.prototype.performStep =
+function(stepOutputMap, shaderProgram) {
+
+	var vertexTransformer = new glslunit.compiler.VariableMinifier(this.minifyPublicVariables_);
+	vertexTransformer.currentNameGenerator_ = globalNameGenerator;
+	vertexTransformer.setShaderProgram(shaderProgram);
+	shaderProgram.vertexAst = vertexTransformer.transformNode(shaderProgram.vertexAst);
+	globalNameGenerator = vertexTransformer.currentNameGenerator_;
+
+	// No need for fragmentTransformer stuff - gl2js passes everything as a
+	//   vertex shader and works just as fine.
+
+	return {
+		'vertexMaxId': vertexTransformer.maxNameId_,
+		'fragmentMaxId': 0
+	};
+};
+
+// Override beforeTransformRoot so it keep all globals (not just varyings and uniforms)
+//   in the variable map. Otherwise these non-varying, non-uniform variables would be
+//   skipped from the global name generator map.
+glslunit.compiler.VariableMinifier.prototype.beforeTransformRoot =
+function(node) {
+	this.structDeclaratorNodes_ = glslunit.compiler.Utils.getStructDeclarations(node);
+	var globals = glslunit.VariableScopeVisitor.getVariablesInScope(node, node, true);
+	for (var globalName in globals) {
+		var declaratorNode = globals[globalName];
+		if (this.shouldRenameNode_(declaratorNode)) {
+			this.currentNameGenerator_.shortenSymbol(globalName);
+		}
+	}
+	this.pushStack_(node);
+};
+
+var steps = {
+	preprocessor: new glslunit.compiler.GlslPreprocessor([], ['GL_ES 1'], true, true),
+	reduceBraces: new glslunit.compiler.BraceReducer(),
+	minifyConstructors: new glslunit.compiler.ConstructorMinifier(),
+// 	deadFunctions: new glslunit.compiler.DeadFunctionRemover(),
+	consolidate: new glslunit.compiler.DeclarationConsolidation(true),
+	minifyVariables: new glslunit.compiler.VariableMinifier(false),
+// 	minifyFunctions: new glslunit.compiler.FunctionMinifier()
+};
+
+
 function minifyGlsl(code) {
 
 	var shaderProgram = new glslunit.compiler.ShaderProgram();
@@ -8,21 +59,11 @@ function minifyGlsl(code) {
 	shaderProgram.fragmentAst = glslunit.glsl.parser.parse('');
 
 	var compiler = new glslunit.compiler.Compiler(shaderProgram);
-	compiler.registerStep(glslunit.compiler.Compiler.CompilerPhase.MINIFICATION,
-	                      new glslunit.compiler.GlslPreprocessor(
-	                          [], ['GL_ES 1'], true, true));
-	compiler.registerStep(glslunit.compiler.Compiler.CompilerPhase.MINIFICATION,
-	                      new glslunit.compiler.BraceReducer());
-	compiler.registerStep(glslunit.compiler.Compiler.CompilerPhase.MINIFICATION,
-	                      new glslunit.compiler.ConstructorMinifier());
-// 	compiler.registerStep(glslunit.compiler.Compiler.CompilerPhase.MINIFICATION,
-// 	                      new glslunit.compiler.DeadFunctionRemover());
-	compiler.registerStep(glslunit.compiler.Compiler.CompilerPhase.MINIFICATION,
-	                      new glslunit.compiler.DeclarationConsolidation(true));
-	compiler.registerStep(glslunit.compiler.Compiler.CompilerPhase.MINIFICATION,
-	                      new glslunit.compiler.VariableMinifier(false));
-// 	compiler.registerStep(glslunit.compiler.Compiler.CompilerPhase.MINIFICATION,
-// 	                      new glslunit.compiler.FunctionMinifier());
+
+	var phase = glslunit.compiler.Compiler.CompilerPhase.MINIFICATION;
+	for (var i in steps) {
+		compiler.registerStep(phase, steps[i]);
+	}
 
 	shaderProgram = compiler.compileProgram();
 
@@ -42,11 +83,12 @@ function gl2js ( inputdir, outputdir, options ) {
 	return sander.lsr( inputdir ).then( function ( filenames ) {
 
 		return mapSeries( filenames, function ( filename ) {
-			console.log('gl2js: ', filename, '→', filename.replace(pattern, '.js'));
+// 			console.log('gl2js: ', filename, '→', filename.replace(pattern, '.js'));
 			if (! filename.match(pattern)) return;
 
 			return sander.readFile(inputdir, filename).then(function(code){
 				var minified = jsesc(minifyGlsl(code.toString()), {wrap: true});
+
 				if (options.format === 'module') {
 					minified = 'module.exports = ' + minified + ';\n';
 				} else {	// variable
